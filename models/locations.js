098bf26c2ef33981ec167ac8c2aced348e0b8cd1
ewill20 <ewill2020@gmail.com>
2019-07-29 20:58:16 -0500

    fuck this

--- models/locations.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-	var Locations = sequelize.define('Locations', {
-		city: DataTypes.STRING,
-		state: DataTypes.STRING,
-		country: DataTypes.STRING
-	});
-	return Locations;
-};
